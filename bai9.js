77cad2305646cca6e9ab4f4a796b525c304f998d
iamgayyyyy <pdatk881@gmail.com>
2025-03-03 00:49:04 +0700

    add

--- bai9.js
+++ bai9.js
@@ -1,3 +1,10 @@
-let hour = prompt("nhap gio : ");
-let minute = prompt("nhap phut : ");
-let second = prompt("nhap giay");
+let hour = Number(prompt("Nhập vào giờ:"));
+let minute = Number(prompt("Nhập vào phút :"));
+let second = Number(prompt("Nhập vào giây:"));
+if (hour <= 12) {
+    alert(hour + ":" + minute + ":" + second + " AM");
+} else if (hour > 12 && hour < 24) {
+    alert(hour - 12 + ":" + minute + ":" + second + " PM");
+} else {
+    alert("Giờ không hợp lệ");
+}
